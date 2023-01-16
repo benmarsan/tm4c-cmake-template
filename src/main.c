5245ba4c6b391e6fae254816ae057ebb6b5f7b17
Ben Marsan <bmarsan@utexas.edu>
2023-01-16 15:30:31 -0600

    Enable printf over UART port
    Basic libc syscall implementation from https://interrupt.memfault.com/blog/boostrapping-libc-with-newlib

--- src/main.c
+++ src/main.c
@@ -30,10 +30,95 @@
 #include <stdint.h>
 #include <math.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <stdio.h>
 #include "inc/tm4c123gh6pm.h"
 
 #include "PLL.h"
 #include "SysTick.h"
+#include "UART.h"
+
+// LIBC SYSCALLS
+/////////////////////
+
+extern int _end;
+
+void *_sbrk(int incr) {
+  static unsigned char *heap = NULL;
+  unsigned char *prev_heap;
+
+  if (heap == NULL) {
+    heap = (unsigned char *)&_end;
+  }
+  prev_heap = heap;
+
+  heap += incr;
+
+  return prev_heap;
+}
+
+int _close(int file) {
+  (void) file;
+  return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+  (void) file;
+  st->st_mode = S_IFCHR;
+  return 0;
+}
+
+int _isatty(int file) {
+  (void) file;
+  return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+  (void) file; (void) ptr; (void) dir;
+  return 0;
+}
+
+void _exit(int status) {
+  (void) status;
+  __asm("BKPT #0");
+}
+
+void _kill(int pid, int sig) {
+  (void) pid; (void) sig;
+  return;
+}
+
+int _getpid(void) {
+  return -1;
+}
+
+int _write (int file, char * ptr, int len) {
+  int written = 0;
+
+  if ((file != 1) && (file != 2) && (file != 3)) {
+    return -1;
+  }
+
+  for (; len != 0; --len) {
+    UART_OutChar(*ptr++);
+    ++written;
+  }
+  return written;
+}
+
+int _read (int file, char * ptr, int len) {
+  int read = 0;
+
+  if (file != 0) {
+    return -1;
+  }
+
+  for (; len > 0; --len) {
+    *(ptr++) = UART_InChar();
+    read++;
+  }
+  return read;
+}
 
 int main(void) {
   SYSCTL_RCGCGPIO_R |= 0x20;    // activate port F
@@ -47,8 +132,15 @@ int main(void) {
   char* a = "testing";
   memcpy(a, "blah", 4);
   GPIO_PORTF_AMSEL_R = 0;  // disable analog functionality on PF
+
+  UART_Init();
+
   while (pow(strlen(a), 2)) {
     GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x04;  // toggle PF2
+
+    // UART_OutString("UART Output\r\n");
+    printf("Hello, world!\r\n");
+
     //    SysTick_Wait(1);        // approximately 720 ns
     //    SysTick_Wait(2);        // approximately 720 ns
     //    SysTick_Wait(10000);    // approximately 0.2 ms
